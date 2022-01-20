87fb5adc841d67e45a8e9db1a3adefbab14d1e91
csl <3079625093@qq.com>
2022-01-20 17:44:33 +0800

    examples

---
+++ example/genEnum.h
@@ -0,0 +1,46 @@
+/**
+ * @brief cmd ['enumgenor Color red yellow green black white blue']
+ */
+
+#include <iostream>
+
+enum class Color
+{
+    /**
+     * @brief options
+     */
+    RED,
+    YELLOW,
+    GREEN,
+    BLACK,
+    WHITE,
+    BLUE
+};
+/**
+ * @brief override operator '<<' for type 'Color'
+ */
+std::ostream &operator<<(std::ostream &os, const Color &obj)
+{
+    switch (obj)
+    {
+    case Color::RED:
+        os << "RED";
+        break;
+    case Color::YELLOW:
+        os << "YELLOW";
+        break;
+    case Color::GREEN:
+        os << "GREEN";
+        break;
+    case Color::BLACK:
+        os << "BLACK";
+        break;
+    case Color::WHITE:
+        os << "WHITE";
+        break;
+    case Color::BLUE:
+        os << "BLUE";
+        break;
+    }
+    return os;
+};
---
+++ example/genStruct.h
@@ -0,0 +1,47 @@
+/**
+ * @brief cmd ['structgenor Car provider:std::string price:float weight:double "color:unsigned char"']
+ */
+
+#include <string>
+#include <iostream>
+
+struct Car
+{
+private:
+    /**
+     * @brief the members
+     */
+    std::string _provider;
+    float _price;
+    double _weight;
+    unsigned char _color;
+
+public:
+    /**
+     * @brief construct a new Car object
+     */
+    Car(const std::string &provider, const float &price, const double &weight, const unsigned char &color)
+        : _provider(provider), _price(price), _weight(weight), _color(color) {}
+
+    inline std::string &provider() { return this->_provider; }
+    inline const std::string &provider() const { return this->_provider; }
+
+    inline float &price() { return this->_price; }
+    inline const float &price() const { return this->_price; }
+
+    inline double &weight() { return this->_weight; }
+    inline const double &weight() const { return this->_weight; }
+
+    inline unsigned char &color() { return this->_color; }
+    inline const unsigned char &color() const { return this->_color; }
+};
+/**
+ * @brief override operator '<<' for type 'Car'
+ */
+std::ostream &operator<<(std::ostream &os, const Car &obj)
+{
+    os << '{';
+    os << "'provider': " << obj.provider() << ", 'price': " << obj.price() << ", 'weight': " << obj.weight() << ", 'color': " << obj.color();
+    os << '}';
+    return os;
+}
