82c3028eeb79ba63412ee0a86c14044ff0540709
csl <3079625093@qq.com>
2022-02-07 19:32:08 +0800

    add clang-format

--- src/enumgenor.cpp
+++ src/enumgenor.cpp
@@ -1,118 +1,126 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
 #include <exception>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-#define OUTPUT(argv) std::cout << argv;
-#define OUTPUT_WITH_END(argv) std::cout << argv << std::endl;
-#define INDENT() std::string("    ")
+static std::ofstream *ofs;
 
-void handleParams(int argc, char const *argv[])
-{
-    bool isHelp = false;
-    if (argc > 1)
-        isHelp = std::string(argv[1]) == std::string("--help") ||
-                 std::string(argv[1]) == std::string("-H");
-    if (argc == 1 || (argc == 2 && !isHelp))
-    {
-        std::cout << "\nThis simple program is used to generate cpp 'enum' source code.\n\n";
-        std::cout << "Usage: myenumGenor EnumName Option1 [Option2] ...\n\n";
-        throw std::runtime_error("");
-    }
-    if (isHelp)
-    {
-        std::cout << "This simple program is used to generate cpp 'struct' source code.\n\n";
-        std::cout << "Usage: myenumGenor EnumName Option1 [Option2] ...\n\n";
-        std::cout << "Here is an example:\n\n";
-        std::cout << "typing 'myenumGenor Color red green blue' will generate code:\n\n";
-        std::cout << "enum class Color"
-                     "\n{"
-                     "\n    /**"
-                     "\n     * @brief options"
-                     "\n     */"
-                     "\n    RED,"
-                     "\n    GREEN,"
-                     "\n    BLUE"
-                     "\n};"
-                     "\n/**"
-                     "\n * @brief override operator '<<' for type 'Color'"
-                     "\n */"
-                     "\nstd::ostream &operator<<(std::ostream &os, const Color &obj)"
-                     "\n{"
-                     "\n    switch (obj)"
-                     "\n    {"
-                     "\n    case Color::RED:"
-                     "\n        os << \" RED \";"
-                     "\n        break;"
-                     "\n    case Color::GREEN:"
-                     "\n        os << \" GREEN \";"
-                     "\n        break;"
-                     "\n    case Color::BLUE:"
-                     "\n        os << \" BLUE \";"
-                     "\n        break;"
-                     "\n    }"
-                     "\n    return os;"
-                     "\n};"
-                  << std::endl;
-        throw std::runtime_error("");
-    }
+#define OUTPUT(argv) (*ofs) << argv;
+#define OUTPUT_WITH_END(argv) (*ofs) << argv << std::endl;
+
+void handleParams(int argc, char const *argv[]) {
+  bool isHelp = false;
+  if (argc > 1)
+    isHelp = std::string(argv[1]) == std::string("--help") ||
+             std::string(argv[1]) == std::string("-H");
+  if (argc == 1 || (argc == 2 && !isHelp)) {
+    std::cout << "\nThis simple program is used to generate cpp 'enum' source "
+                 "code.\n\n";
+    std::cout << "Usage: myenumGenor EnumName Option1 [Option2] ...\n\n";
+    throw std::runtime_error("");
+  }
+  if (isHelp) {
+    std::cout << "This simple program is used to generate cpp 'struct' source "
+                 "code.\n\n";
+    std::cout << "Usage: myenumGenor EnumName Option1 [Option2] ...\n\n";
+    std::cout << "Here is an example:\n\n";
+    std::cout
+        << "typing 'myenumGenor Color red green blue' will generate code:\n\n";
+    std::cout
+        << "enum class Color"
+           "\n{"
+           "\n    /**"
+           "\n     * @brief options"
+           "\n     */"
+           "\n    RED,"
+           "\n    GREEN,"
+           "\n    BLUE"
+           "\n};"
+           "\n/**"
+           "\n * @brief override operator '<<' for type 'Color'"
+           "\n */"
+           "\nstd::ostream &operator<<(std::ostream &os, const Color &obj)"
+           "\n{"
+           "\n    switch (obj)"
+           "\n    {"
+           "\n    case Color::RED:"
+           "\n        os << \" RED \";"
+           "\n        break;"
+           "\n    case Color::GREEN:"
+           "\n        os << \" GREEN \";"
+           "\n        break;"
+           "\n    case Color::BLUE:"
+           "\n        os << \" BLUE \";"
+           "\n        break;"
+           "\n    }"
+           "\n    return os;"
+           "\n};"
+        << std::endl;
+    throw std::runtime_error("");
+  }
 }
 
-std::pair<std::string, std::vector<std::string>> init(int argc, char const *argv[])
-{
-    std::vector<std::string> options;
-    std::string enumName = argv[1];
-    for (int i = 2; i != argc; ++i)
-    {
-        std::string option = argv[i];
-        std::for_each(option.begin(), option.end(), [](char &c) -> void
-                      { c = std::toupper(c); });
-        options.push_back(option);
-    }
-    return {enumName, options};
+std::pair<std::string, std::vector<std::string>> init(int argc,
+                                                      char const *argv[]) {
+  std::vector<std::string> options;
+  std::string enumName = argv[1];
+  for (int i = 2; i != argc; ++i) {
+    std::string option = argv[i];
+    std::for_each(option.begin(), option.end(),
+                  [](char &c) -> void { c = std::toupper(c); });
+    options.push_back(option);
+  }
+  return {enumName, options};
 }
 
-void genEnum(const std::string &enumName, const std::vector<std::string> &options)
-{
-    OUTPUT_WITH_END("enum class " + enumName)
-    OUTPUT_WITH_END("{")
-    OUTPUT_WITH_END(INDENT() + "/**")
-    OUTPUT_WITH_END(INDENT() + " * @brief options")
-    OUTPUT_WITH_END(INDENT() + " */")
-    for (int i = 0; i != options.size() - 1; ++i)
-        OUTPUT_WITH_END(INDENT() + options.at(i) + ",")
-    OUTPUT_WITH_END(INDENT() + options.back())
-    OUTPUT_WITH_END("};")
-    OUTPUT_WITH_END("/**")
-    OUTPUT_WITH_END(" * @brief override operator '<<' for type '" + enumName + "'")
-    OUTPUT_WITH_END(" */")
-    OUTPUT_WITH_END("std::ostream &operator<<(std::ostream &os, const " + enumName + " &obj)")
-    OUTPUT_WITH_END("{")
-    OUTPUT_WITH_END(INDENT() + "switch (obj)")
-    OUTPUT_WITH_END(INDENT() + "{")
-    for (const auto &elem : options)
-    {
-        OUTPUT_WITH_END(INDENT() + "case " + enumName + "::" + elem + ":")
-        OUTPUT_WITH_END(INDENT() + INDENT() + "os << \"" + elem + "\";")
-        OUTPUT_WITH_END(INDENT() + INDENT() + "break;")
-    }
-    OUTPUT_WITH_END(INDENT() + "}")
-    OUTPUT_WITH_END(INDENT() + "return os;")
-    OUTPUT_WITH_END("};")
+void genEnum(const std::string &enumName,
+             const std::vector<std::string> &options) {
+  OUTPUT_WITH_END("enum class " + enumName)
+  OUTPUT_WITH_END("{")
+  OUTPUT_WITH_END("/**")
+  OUTPUT_WITH_END(" * @brief options")
+  OUTPUT_WITH_END(" */")
+  for (int i = 0; i != options.size() - 1; ++i)
+    OUTPUT_WITH_END(options.at(i) + ",")
+  OUTPUT_WITH_END(options.back())
+  OUTPUT_WITH_END("};")
+  OUTPUT_WITH_END("/**")
+  OUTPUT_WITH_END(" * @brief override operator '<<' for type '" + enumName +
+                  "'")
+  OUTPUT_WITH_END(" */")
+  OUTPUT_WITH_END("std::ostream &operator<<(std::ostream &os, const " +
+                  enumName + " &obj)")
+  OUTPUT_WITH_END("{")
+  OUTPUT_WITH_END("switch (obj)")
+  OUTPUT_WITH_END("{")
+  for (const auto &elem : options) {
+    OUTPUT_WITH_END("case " + enumName + "::" + elem + ":")
+    OUTPUT_WITH_END("os << \"" + elem + "\";")
+    OUTPUT_WITH_END("break;")
+  }
+  OUTPUT_WITH_END("}")
+  OUTPUT_WITH_END("return os;")
+  OUTPUT_WITH_END("};")
 }
 
-int main(int argc, char const *argv[])
-{
-    try
-    {
-        handleParams(argc, argv);
-        auto [enumName, options] = init(argc, argv);
-        genEnum(enumName, options);
-    }
-    catch (const std::runtime_error &e)
-    {
-        return 0;
-    }
+int runCommand(const std::string &cmd) { return system(cmd.c_str()); }
+
+int main(int argc, char const *argv[]) {
+  ::ofs = new std::ofstream("/home/csl/StudyCPP/codeGenor/example/temp.log");
+
+  try {
+    handleParams(argc, argv);
+    auto [enumName, options] = init(argc, argv);
+    genEnum(enumName, options);
+  } catch (const std::runtime_error &e) {
     return 0;
+  }
+  ofs->close();
+  delete ofs;
+  ::runCommand(
+      "clang-format /home/csl/StudyCPP/codeGenor/example/temp.log "
+      "--style=Google");
+  return 0;
 }
--- src/structgenor.cpp
+++ src/structgenor.cpp
@@ -1,155 +1,163 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
 #include <exception>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-#define OUTPUT(argv) std::cout << argv;
-#define OUTPUT_WITH_END(argv) std::cout << argv << std::endl;
-#define INDENT() std::string("    ")
+static std::ofstream *ofs;
 
-struct MemInfo
-{
-    std::string _varName;
-    std::string _varType;
+#define OUTPUT(argv) (*ofs) << argv;
+#define OUTPUT_WITH_END(argv) (*ofs) << argv << std::endl;
+
+struct MemInfo {
+  std::string _varName;
+  std::string _varType;
 };
 
-MemInfo split(const std::string &str)
-{
-    MemInfo info;
-    auto pos = str.find_first_of(':');
-    info._varName = str.substr(0, pos);
-    info._varType = str.substr(pos + 1);
-    return info;
+MemInfo split(const std::string &str) {
+  MemInfo info;
+  auto pos = str.find_first_of(':');
+  info._varName = str.substr(0, pos);
+  info._varType = str.substr(pos + 1);
+  return info;
 }
 
-std::pair<std::string, std::vector<MemInfo>> init(int argc, char const *argv[])
-{
-    std::string structName = argv[1];
+std::pair<std::string, std::vector<MemInfo>> init(int argc,
+                                                  char const *argv[]) {
+  std::string structName = argv[1];
 
-    std::vector<MemInfo> info(argc - 2);
+  std::vector<MemInfo> info(argc - 2);
 
-    for (int i = 2; i != argc; ++i)
-        info.at(i - 2) = split(argv[i]);
+  for (int i = 2; i != argc; ++i) info.at(i - 2) = split(argv[i]);
 
-    return std::make_pair(structName, info);
+  return std::make_pair(structName, info);
 }
 
-void handleParams(int argc, char const *argv[])
-{
-    bool isHelp = false;
-    if (argc > 1)
-        isHelp = std::string(argv[1]) == std::string("--help") ||
-                 std::string(argv[1]) == std::string("-H");
-    if (argc == 1 || (argc == 2 && !isHelp))
-    {
-        std::cout << "\nThis simple program is used to generate cpp 'struct' source code.\n\n";
-        std::cout << "Usage: mystructGenor StructName MemName1:MemType1 [MemName2:MemType2] ...\n\n";
-        throw std::runtime_error("");
-    }
-    if (isHelp)
-    {
-        std::cout << "This simple program is used to generate cpp 'struct' source code.\n\n";
-        std::cout << "Usage: mystructGenor StructName MemName1:MemType1 [MemName2:MemType2] ...\n\n";
-        std::cout << "Here is an example:\n\n";
-        std::cout << "typing 'mystructGenor Info id:uint' will generate code:\n\n";
-        std::cout << "struct Info"
-                     "\n{"
-                     "\nprivate:"
-                     "\n    /**"
-                     "\n     * @brief the members"
-                     "\n     */"
-                     "\n    uint _id;"
-                     "\n"
-                     "\npublic:"
-                     "\n    /**"
-                     "\n     * @brief construct a new Info object"
-                     "\n     */"
-                     "\n    Info(const uint &id)"
-                     "\n        : _id(id) {}"
-                     "\n"
-                     "\n    inline uint &id() { return this->_id; }"
-                     "\n    inline const uint &id() const { return this->_id; }"
-                     "\n"
-                     "\n};"
-                     "\n/**"
-                     "\n * @brief override operator '<<' for type 'Info'"
-                     "\n */"
-                     "\nstd::ostream &operator<<(std::ostream &os, const Info &obj)"
-                     "\n{"
-                     "\n    os << '{';"
-                     "\n    os << \" 'id' : \" << obj.id();"
-                     "\n    os << '}';"
-                     "\n    return os;"
-                     "\n}"
-                  << std::endl;
-        throw std::runtime_error("");
-    }
+void handleParams(int argc, char const *argv[]) {
+  bool isHelp = false;
+  if (argc > 1)
+    isHelp = std::string(argv[1]) == std::string("--help") ||
+             std::string(argv[1]) == std::string("-H");
+  if (argc == 1 || (argc == 2 && !isHelp)) {
+    std::cout << "\nThis simple program is used to generate cpp 'struct' "
+                 "source code.\n\n";
+    std::cout << "Usage: mystructGenor StructName MemName1:MemType1 "
+                 "[MemName2:MemType2] ...\n\n";
+    throw std::runtime_error("");
+  }
+  if (isHelp) {
+    std::cout << "This simple program is used to generate cpp 'struct' source "
+                 "code.\n\n";
+    std::cout << "Usage: mystructGenor StructName MemName1:MemType1 "
+                 "[MemName2:MemType2] ...\n\n";
+    std::cout << "Here is an example:\n\n";
+    std::cout << "typing 'mystructGenor Info id:uint' will generate code:\n\n";
+    std::cout << "struct Info"
+                 "\n{"
+                 "\nprivate:"
+                 "\n    /**"
+                 "\n     * @brief the members"
+                 "\n     */"
+                 "\n    uint _id;"
+                 "\n"
+                 "\npublic:"
+                 "\n    /**"
+                 "\n     * @brief construct a new Info object"
+                 "\n     */"
+                 "\n    Info(const uint &id)"
+                 "\n        : _id(id) {}"
+                 "\n"
+                 "\n    inline uint &id() { return this->_id; }"
+                 "\n    inline const uint &id() const { return this->_id; }"
+                 "\n"
+                 "\n};"
+                 "\n/**"
+                 "\n * @brief override operator '<<' for type 'Info'"
+                 "\n */"
+                 "\nstd::ostream &operator<<(std::ostream &os, const Info &obj)"
+                 "\n{"
+                 "\n    os << '{';"
+                 "\n    os << \" 'id' : \" << obj.id();"
+                 "\n    os << '}';"
+                 "\n    return os;"
+                 "\n}"
+              << std::endl;
+    throw std::runtime_error("");
+  }
 }
 
-void genStruct(const std::string &structName, const std::vector<MemInfo> &info)
-{
-    OUTPUT_WITH_END("struct " + structName)
-    OUTPUT_WITH_END("{")
-    OUTPUT_WITH_END("private:")
-    OUTPUT_WITH_END(INDENT() + "/**")
-    OUTPUT_WITH_END(INDENT() + " * @brief the members")
-    OUTPUT_WITH_END(INDENT() + " */")
-    for (const auto &elem : info)
-        OUTPUT_WITH_END(INDENT() + elem._varType + " _" + elem._varName + ";")
+void genStruct(const std::string &structName,
+               const std::vector<MemInfo> &info) {
+  OUTPUT_WITH_END("struct " + structName)
+  OUTPUT_WITH_END("{")
+  OUTPUT_WITH_END("private:")
+  OUTPUT_WITH_END("/**")
+  OUTPUT_WITH_END(" * @brief the members")
+  OUTPUT_WITH_END(" */")
+  for (const auto &elem : info)
+    OUTPUT_WITH_END(elem._varType + " _" + elem._varName + ";")
+  OUTPUT_WITH_END("")
+  OUTPUT_WITH_END("public:")
+  OUTPUT_WITH_END("/**")
+  OUTPUT_WITH_END(" * @brief construct a new " + structName + " object")
+  OUTPUT_WITH_END(" */")
+  OUTPUT(structName + "(")
+  for (int i = 0; i != info.size() - 1; ++i)
+    OUTPUT("const " + info.at(i)._varType + " &" + info.at(i)._varName + ", ")
+  OUTPUT("const " + info.back()._varType + " &" + info.back()._varName)
+  OUTPUT_WITH_END(")")
+  OUTPUT(": ")
+  for (int i = 0; i != info.size() - 1; ++i)
+    OUTPUT("_" + info.at(i)._varName + "(" + info.at(i)._varName + "), ")
+  OUTPUT("_" + info.back()._varName + "(" + info.back()._varName + ")")
+  OUTPUT_WITH_END(" {}")
+  OUTPUT_WITH_END("")
+  for (const auto &elem : info) {
+    OUTPUT_WITH_END("inline " + elem._varType + " &" + elem._varName +
+                    "() { return this->_" + elem._varName + "; }")
+    OUTPUT_WITH_END("inline const " + elem._varType + " &" + elem._varName +
+                    "() const { return this->_" + elem._varName + "; }")
     OUTPUT_WITH_END("")
-    OUTPUT_WITH_END("public:")
-    OUTPUT_WITH_END(INDENT() + "/**")
-    OUTPUT_WITH_END(INDENT() + " * @brief construct a new " + structName + " object")
-    OUTPUT_WITH_END(INDENT() + " */")
-    OUTPUT(INDENT() + structName + "(")
-    for (int i = 0; i != info.size() - 1; ++i)
-        OUTPUT("const " + info.at(i)._varType + " &" + info.at(i)._varName + ", ")
-    OUTPUT("const " + info.back()._varType + " &" + info.back()._varName)
-    OUTPUT_WITH_END(")")
-    OUTPUT(INDENT() + INDENT() + ": ")
-    for (int i = 0; i != info.size() - 1; ++i)
-        OUTPUT("_" + info.at(i)._varName + "(" + info.at(i)._varName + "), ")
-    OUTPUT("_" + info.back()._varName + "(" + info.back()._varName + ")")
-    OUTPUT_WITH_END(" {}")
-    OUTPUT_WITH_END("")
-    for (const auto &elem : info)
-    {
-        OUTPUT_WITH_END(INDENT() + "inline " + elem._varType + " &" + elem._varName + "() { return this->_" + elem._varName + "; }")
-        OUTPUT_WITH_END(INDENT() + "inline const " + elem._varType + " &" + elem._varName + "() const { return this->_" + elem._varName + "; }")
-        OUTPUT_WITH_END("")
-    }
-    OUTPUT_WITH_END("};")
-    OUTPUT_WITH_END("/**")
-    OUTPUT_WITH_END(" * @brief override operator '<<' for type '" + structName + "'")
-    OUTPUT_WITH_END(" */")
-    OUTPUT_WITH_END("std::ostream &operator<<(std::ostream &os, const " + structName + " &obj)")
-    OUTPUT_WITH_END("{")
-    OUTPUT_WITH_END(INDENT() + "os << '{';");
-    OUTPUT(INDENT() + "os ")
-    OUTPUT(std::string("<< ") + "\"'" + info.front()._varName + "': \" << obj." + info.front()._varName + "()")
-    if (info.size() == 1)
-        OUTPUT(";")
-    for (int i = 1; i != info.size(); ++i)
-        OUTPUT(std::string(" << ") + "\", '" + info.at(i)._varName + "': \" << obj." + info.at(i)._varName + "()")
-    OUTPUT_WITH_END(";")
-    OUTPUT_WITH_END(INDENT() + "os << '}';");
-    OUTPUT_WITH_END(INDENT() + "return os;")
-    OUTPUT_WITH_END("}")
+  }
+  OUTPUT_WITH_END("};")
+  OUTPUT_WITH_END("/**")
+  OUTPUT_WITH_END(" * @brief override operator '<<' for type '" + structName +
+                  "'")
+  OUTPUT_WITH_END(" */")
+  OUTPUT_WITH_END("std::ostream &operator<<(std::ostream &os, const " +
+                  structName + " &obj)")
+  OUTPUT_WITH_END("{")
+  OUTPUT_WITH_END("os << '{';");
+  OUTPUT("os ")
+  OUTPUT(std::string("<< ") + "\"'" + info.front()._varName + "': \" << obj." +
+         info.front()._varName + "()")
+  for (int i = 1; i != info.size(); ++i)
+    OUTPUT(std::string(" << ") + "\", '" + info.at(i)._varName +
+           "': \" << obj." + info.at(i)._varName + "()")
+  OUTPUT_WITH_END(";")
+  OUTPUT_WITH_END("os << '}';");
+  OUTPUT_WITH_END("return os;")
+  OUTPUT_WITH_END("}")
 
-    return;
+  return;
 }
 
-int main(int argc, char const *argv[])
-{
-    try
-    {
-        handleParams(argc, argv);
-        auto [structName, info] = init(argc, argv);
-        genStruct(structName, info);
-    }
-    catch (const std::runtime_error &e)
-    {
-        return 0;
-    }
+int runCommand(const std::string &cmd) { return system(cmd.c_str()); }
+
+int main(int argc, char const *argv[]) {
+  ::ofs = new std::ofstream("/home/csl/StudyCPP/codeGenor/example/temp.log");
+  try {
+    handleParams(argc, argv);
+    auto [structName, info] = init(argc, argv);
+    genStruct(structName, info);
+  } catch (const std::runtime_error &e) {
+    return 0;
+  }
+  ofs->close();
+  delete ofs;
+  ::runCommand(
+      "clang-format /home/csl/StudyCPP/codeGenor/example/temp.log "
+      "--style=Google");
+  return 0;
 }
